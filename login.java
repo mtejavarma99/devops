0dcd50dc6f2470a8c29f14050400b10d8ffef232
mtejavarma99 <mtejavarma99@gmail.com>
2023-01-19 16:04:07 +0530

    Content updated

--- login.java
+++ login.java
@@ -1,6 +1,11 @@
-Hi My name is teja, 
-working at xcelpros as an associate 
+Hi My name is teja,
+working at xcelpros as an associate
 thank you
-
+bye
 Now, I'm going to learn devops course
 and now I want to practice it
+Hello Vijay how are you
+I'm in banglore
+when are you coming.
+Learning DevOps
+Are you coming to banglore
