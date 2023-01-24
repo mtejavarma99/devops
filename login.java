57e9bb4b9631dbcb8868524158180c03b8fa565f
mtejavarma99 <mtejavarma99@gmail.com>
2023-01-24 17:13:39 +0530

    new line added

--- login.java
+++ login.java
@@ -6,6 +6,7 @@ Now, I'm going to learn devops course
 and now I want to practice it
 Hello Vijay how are you buddy
 I'm in banglore
+i'm in ballari
 when are you coming.
 Learning DevOps
 is it easy to learn
